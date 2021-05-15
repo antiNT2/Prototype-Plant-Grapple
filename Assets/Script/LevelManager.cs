b0c16cc2c29dc161349d6006f71497089838826b
antiNT <32984684+antiNT2@users.noreply.github.com>
2021-05-15 20:37:48 +0200

    started builduing first level prototype + added text mesh pro

---
+++ Assets/Script/LevelManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public List<GameLevel> allLevels = new List<GameLevel>();
+    public SceneField mainScene;
+    bool mainSceneIsLoaded;
+
+    private void Awake()
+    {
+        int levelToLoad = PlayerPrefs.GetInt("LevelToLoad");
+        LoadLevel(levelToLoad);
+    }
+
+    public void LoadLevel(int levelIndex)
+    {
+        GetComponent<Camera>().enabled = false;
+        if (mainSceneIsLoaded == false)
+            SceneManager.LoadScene(mainScene, LoadSceneMode.Additive);
+
+        SceneManager.LoadScene(allLevels[levelIndex].levelScene, LoadSceneMode.Additive);
+    }
+}
+
+[System.Serializable]
+public class GameLevel
+{
+    public string levelName;
+    public SceneField levelScene;
+}
+
